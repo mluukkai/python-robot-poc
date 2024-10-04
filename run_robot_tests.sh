29434a486f491a05cb246c3d86fdf7ac64ab15e4
Matti Luukkainen <mluukkai@iki.fi>
2024-10-04 22:16:57 +0300

    debug

--- run_robot_tests.sh
+++ run_robot_tests.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
+echo "Running tests"
+
 # käynnistetään Flask-palvelin taustalle
 poetry run python3 app.py &
+
+echo "started Flask server"
 
 # odetetaan, että palvelin on valmiina ottamaan vastaan pyyntöjä
 while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:5000/ping)" != "200" ]];
   do sleep 1;
 done
 
+echo "Flask server is ready"
+
 # suoritetaan testit
 poetry run robot src/tests
 
 status=$?
 
-# pysäytetään Flask-palvelin portissa 5001
+# pysäytetään Flask-palvelin portissa 5000
 kill $(lsof -t -i:5000)
 
 exit $status
