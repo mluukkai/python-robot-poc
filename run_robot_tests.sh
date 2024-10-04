0ea10c0835ba1aab9f485384f033d9f02b62639b
Matti Luukkainen <mluukkai@iki.fi>
2024-10-04 21:49:15 +0300

    fix robot

--- run_robot_tests.sh
+++ run_robot_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # käynnistetään Flask-palvelin taustalle
-poetry run python3 src/index.py &
+poetry run python3 app.py &
 
 # odetetaan, että palvelin on valmiina ottamaan vastaan pyyntöjä
 while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:5000/ping)" != "200" ]];
