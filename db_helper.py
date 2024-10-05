86368adfd08dc0f43ca687168cd7b7f9df01091d
Matti Luukkainen <mluukkai@iki.fi>
2024-10-05 19:12:28 +0300

    betterment

--- db_helper.py
+++ db_helper.py
@@ -1,26 +1,40 @@
 from setup import db
 from sqlalchemy import text
 
+table_name = "messages"
+
+def table_exists(name):
+  sql_table_existence = text(
+    "SELECT EXISTS ("
+    "  SELECT 1"
+    "  FROM information_schema.tables"
+    f" WHERE table_name = '{name}'"
+    ")"
+  )
+
+  result = db.session.execute(sql_table_existence)
+  return result.fetchall()[0][0]
+
 def reset_db():
-  sql = text("DELETE FROM messages")
+  print(f"Clearing contents from table {table_name}")
+  sql = text(f"DELETE FROM {table_name}")
   db.session.execute(sql)
   db.session.commit()
 
 def setup_db():
-  #try:
-  #  sql = text("DROP TABLE messages")
-  #  db.session.execute(sql)
-  #  db.session.commit()
-  #except:
-  #  pass
+  if table_exists(table_name):
+    print(f"Table {table_name} exists, dropping")
+    sql = text(f"DROP TABLE {table_name}")
+    db.session.execute(sql)
+    db.session.commit()
 
-  command = """
-  CREATE TABLE messages (
-    id SERIAL PRIMARY KEY,
-    content TEXT NOT NULL
-  );
-  """
+  print(f"Creating table {table_name}")
+  sql = text(
+    f"CREATE TABLE {table_name} ("
+    "  id SERIAL PRIMARY KEY, "
+    "  content TEXT NOT NULL"
+    ")"
+  )
 
-  sql = text(command)
   db.session.execute(sql)
   db.session.commit()
